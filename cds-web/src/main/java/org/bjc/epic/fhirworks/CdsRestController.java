1d18a1438243293f09be0827e03076370797b7b5
Kevin Heard <kevin.heard@bjc.org>
2017-03-16 18:13:32 -0500

    Updates

--- cds-web/src/main/java/org/bjc/epic/fhirworks/CdsRestController.java
+++ cds-web/src/main/java/org/bjc/epic/fhirworks/CdsRestController.java
@@ -3,15 +3,20 @@ package org.bjc.epic.fhirworks;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Map;
 
 @Controller
 public class CdsRestController
 {
 
     @RequestMapping("/hello")
-    public String helloWorld(Model model)
+    public String helloWorld(@RequestParam Map<String,String> params, Model model)
     {
+
         model.addAttribute("message", "Hello!");
+        model.addAttribute("params", params);
         return "hello";
     }
 
